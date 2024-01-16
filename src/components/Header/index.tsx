659b8bf9a0725273fae4112b5ad795a2c93a39c0
Caio Souza <souzsdev@gmail.com>
2024-01-16 13:40:39 -0300

    Header - layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Logo from "../Logo";
+
+const navItems = [
+  {
+    name: "Sobre",
+    href: "/about",
+  },
+  {
+    name: "Expieriências",
+    href: "/experiences",
+  },
+  {
+    name: "Projetos",
+    href: "/projects",
+  },
+  {
+    name: "Conhecimentos",
+    href: "/skills",
+  }
+]
+
+export default function Header() {
+  return (
+    <header className="bg-midnightExpresso text-white">
+      <div className="h-24 max-w-7xl mx-auto px-8 flex justify-between items-center">
+        <Logo />
+        <nav>
+          <ul className="flex gap-x-12 justify-center items-center text-lg">
+            {
+              navItems.map(({name, href}) => {
+                return (
+                  <li key={`nav-item-#${href}`}>
+                    <Link href={href} className="relative after:absolute after:w-0 after:h-0.5 after:bg-skyBlaze after:block after:rounded-full after:mt-1 after:duration-300 hover:after:w-full">
+                      {name}
+                    </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="text-3xl text-white font-semibold tracking-wider" title="Logo do portfolio.">
+      SOU
+      <span className="text-oceanLight">Z</span>
+      S
+    </Link>
+  )
+}
